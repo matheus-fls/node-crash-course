399f2eed616f10488eb265febd1ef7fc1678b074
matheus-fls <matheus_fls@yahoo.com.br>
2021-10-31 20:02:47 -0300

    feat: read file using fs

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,22 +8,28 @@ const path = require("path");
 // });
 
 // Create and write to file
-fs.writeFile(
-  path.join(__dirname, "/test", "hello.txt"),
-  "Hello World!",
-  (err) => {
-    if (err) throw err;
-    console.log("File written to...");
+// fs.writeFile(
+//   path.join(__dirname, "/test", "hello.txt"),
+//   "Hello World!",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File written to...");
 
-    // File append
-    fs.appendFile(
-        path.join(__dirname, "/test", "hello.txt"),
-        " I love Node.js",
-        (err) => {
-          if (err) throw err;
-          console.log("File written to...");
-        }
-      );
-      
-  }
-);
+//     // File append
+//     fs.appendFile(
+//         path.join(__dirname, "/test", "hello.txt"),
+//         " I love Node.js",
+//         (err) => {
+//           if (err) throw err;
+//           console.log("File written to...");
+//         }
+//       );
+
+//   }
+// );
+
+// Read file
+fs.readFile(path.join(__dirname, "/test", "hello.txt"), 'utf-8', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
