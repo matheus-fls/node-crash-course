485a669506418517a3961fb470226b43cbf8c43c
matheus-fls <matheus_fls@yahoo.com.br>
2021-10-31 20:49:32 -0300

    feat: logger with events and uuid

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const Person = require("./person");
+const Logger = require("./logger");
 
-const person1 = new Person("John Doe", 30);
+const logger = new Logger();
 
-person1.greeting();
+logger.on("message", (data) => console.log("Called Listener", data));
+
+logger.log("Hello World");
