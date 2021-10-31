27c0c0f71538546a68bcad492076b5a446f8c65b
matheus-fls <matheus_fls@yahoo.com.br>
2021-10-31 19:51:43 -0300

    feat: write to file with fs module

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,8 +1,18 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // Create folder
-fs.mkdir(path.join(__dirname, '/test'), {}, err => {
-    if(err) throw err;
-    console.log('Folder created...');
-});
+// fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+//   if (err) throw err;
+//   console.log("Folder created...");
+// });
+
+// Create and write to file
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "Hello World!",
+  (err) => {
+    if (err) throw err;
+    console.log("File written to...");
+  }
+);
