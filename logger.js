e7ab02abb86b94c9cb0b1a6cfa1b8c62624a14b1
matheus-fls <matheus_fls@yahoo.com.br>
2021-10-31 20:56:30 -0300

    refactor: remove export

--- logger.js
+++ logger.js
@@ -8,4 +8,10 @@ class Logger extends EventEmitter {
   }
 }
 
-module.exports = Logger;
+const logger = new Logger();
+
+logger.on("message", (data) => console.log("Called Listener", data));
+
+logger.log("Hello World");
+
+// module.exports = Logger;
