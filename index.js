0f888bd8f01964952834a6b90650fd38d92a6843
matheus-fls <matheus_fls@yahoo.com.br>
2021-10-31 19:53:32 -0300

    lint: fix indentation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const Person = require('./person');
+const Person = require("./person");
 
-const person1 = new Person('John Doe', 30);
+const person1 = new Person("John Doe", 30);
 
 person1.greeting();
