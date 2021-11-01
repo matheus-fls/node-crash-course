35a6e812f11035d0a5c643a840652c384501d301
matheus-fls <matheus_fls@yahoo.com.br>
2021-10-31 21:16:05 -0300

    feat: handle about page

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ const server = http.createServer((req, res) => {
       }
     );
   }
+
+  if (req.url === "/about") {
+    fs.readFile(
+      path.join(__dirname, "public", "about.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
 });
 
 const PORT = process.env.PORT || 5000;
