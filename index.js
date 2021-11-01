5a70a8daffd9514f71d915a1c1753cbaedb74bbd
matheus-fls <matheus_fls@yahoo.com.br>
2021-10-31 21:14:28 -0300

    feat: start server and handle homepage

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const Logger = require("./logger");
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
+});
 
-logger.on("message", (data) => console.log("Called Listener", data));
+const PORT = process.env.PORT || 5000;
 
-logger.log("Hello World");
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
